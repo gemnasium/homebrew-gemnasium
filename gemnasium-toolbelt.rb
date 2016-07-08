9c4a122f0a0533c0b72c1790239c011606a08ed0
Philippe Lafoucrière <philippe.lafoucriere@tech-angels.com>
2016-07-07 20:19:21 -0400

    switch to sha256

--- gemnasium-toolbelt.rb
+++ gemnasium-toolbelt.rb
@@ -4,7 +4,7 @@ class GemnasiumToolbelt < Formula
   homepage "https://gemnasium.com"
 
   url "https://github.com/gemnasium/toolbelt/archive/0.2.8.tar.gz"
-  sha1 "771e2bb6b763b6e1e32471ab354af0b797bc94a3"
+  sha256 "5457de7dd9a6d7c9c872a67b3db12a3f2d4c4a7c026e700e5041933c4e52b61a"
 
   depends_on "go" => :build
 
